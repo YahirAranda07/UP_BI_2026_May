8e7770ff13c0c4813684f30a426c73c6afc0ad2f
YahirAranda07 <0286935@up.edu.mx>
2026-05-21 21:10:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import streamlit as st
-
-st.title('Hola Mundo!!!')
+def show_header(text_title: str):
+    # Layout: logo + title side by side
+    col1, col2 = st.columns([1, 6])
+    
+    with col1:
+        st.image("imagenes/UPlogo.jpg", width=200)
+        
+    with col2:
+        st.title(text_title)
+        st.caption("📘 Developed for: *Business Intelligence (Graduate Level)*")
+        st.caption("Instructor: Edgar Avalos-Gauna (2025), Universidad Panamericana")
